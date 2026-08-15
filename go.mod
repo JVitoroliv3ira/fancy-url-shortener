b45068394ea4c6e851cf173c059d2fae73d49458
João Vitor <jvitoroliveira0711@gmail.com>
2026-08-15 13:40:18 -0300

    chore: Altera stack do projeto

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/JVitoroliv3ira/fancy-url-shortener
-
-go 1.25.6
-
-require github.com/google/uuid v1.6.0
--- internal/link/ports/id_generator.go
+++
@@ -1,7 +0,0 @@
-package ports
-
-import "github.com/JVitoroliv3ira/fancy-url-shortener/internal/link/domain"
-
-type IDGenerator interface {
-	Generate() domain.LinkID
-}
