1f7dd684c17ce11144fec6271187f98f7080e455
João Vitor <jvitoroliveira0711@gmail.com>
2026-08-18 22:33:27 -0300

    feat: Adiciona método saveIfAbsent para tratar race condition

--- services/fancy-url-shortener/src/main/java/io/github/jvitoroliv3ira/fancyurlshortener/shortening/domain/repository/ShortUrlRepository.java
+++ services/fancy-url-shortener/src/main/java/io/github/jvitoroliv3ira/fancyurlshortener/shortening/domain/repository/ShortUrlRepository.java
@@ -1,12 +1,7 @@
 package io.github.jvitoroliv3ira.fancyurlshortener.shortening.domain.repository;
 
-import java.util.Optional;
-
 import io.github.jvitoroliv3ira.fancyurlshortener.shortening.domain.entity.ShortUrl;
-import io.github.jvitoroliv3ira.fancyurlshortener.shortening.domain.valueobject.ShortCode;
 
 public interface ShortUrlRepository {
-  boolean existsByShortCode(ShortCode shortCode);
-
-  Optional<ShortUrl> findByShortCode(ShortCode shortCode);
+  boolean saveIfAbsent(ShortUrl shortUrl);
 }
