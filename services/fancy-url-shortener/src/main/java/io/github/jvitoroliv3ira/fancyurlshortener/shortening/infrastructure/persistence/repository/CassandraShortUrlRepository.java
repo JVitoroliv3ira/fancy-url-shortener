3b9870181d6d9d9bed12ed1005c520d42f145335
João Vitor <jvitoroliveira0711@gmail.com>
2026-08-23 21:36:06 -0300

    feat: Implementa metodo insertIfAbsent

--- services/fancy-url-shortener/src/main/java/io/github/jvitoroliv3ira/fancyurlshortener/shortening/infrastructure/persistence/repository/CassandraShortUrlRepository.java
+++ services/fancy-url-shortener/src/main/java/io/github/jvitoroliv3ira/fancyurlshortener/shortening/infrastructure/persistence/repository/CassandraShortUrlRepository.java
@@ -2,12 +2,45 @@ package io.github.jvitoroliv3ira.fancyurlshortener.shortening.infrastructure.per
 
 import org.springframework.stereotype.Repository;
 
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.BoundStatement;
+import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import com.datastax.oss.driver.api.core.cql.Row;
+
 import io.github.jvitoroliv3ira.fancyurlshortener.shortening.domain.entity.ShortUrl;
 import io.github.jvitoroliv3ira.fancyurlshortener.shortening.domain.repository.ShortUrlRepository;
 
 @Repository
 public class CassandraShortUrlRepository implements ShortUrlRepository {
+  private static final String INSERT_IF_ABSENT_QUERY = """
+        INSERT INTO short_urls_by_code (
+          code,
+          original_url,
+          created_at,
+          expires_at
+        )
+        VALUES (?, ?, ?, ?)
+        IF NOT EXISTS
+      """;
+
+  private final CqlSession session;
+  private final PreparedStatement insertIfAbsentStatement;
+
+  public CassandraShortUrlRepository(CqlSession session) {
+    this.session = session;
+    this.insertIfAbsentStatement = session.prepare(INSERT_IF_ABSENT_QUERY);
+  }
+
+  @Override
   public boolean saveIfAbsent(ShortUrl shortUrl) {
-    return true;
+    BoundStatement statement = insertIfAbsentStatement.bind(
+        shortUrl.shortCode().value(),
+        shortUrl.originalUrl().value(),
+        shortUrl.createdAt(),
+        shortUrl.expiresAt());
+
+    Row row = session.execute(statement).one();
+
+    return row != null && row.getBoolean("[applied]");
   }
 }
