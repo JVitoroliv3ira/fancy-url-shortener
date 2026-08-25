5e2dac3b03de2a641288bbcd3228433fcfff642e
João Vitor <jvitoroliveira0711@gmail.com>
2026-08-24 23:24:07 -0300

    feat: Implementa endpoint responsável por redirecionar por ShortCode

---
+++ services/fancy-url-shortener/src/main/java/io/github/jvitoroliv3ira/fancyurlshortener/redirecting/infrastructure/web/ResolveRedirectTargetController.java
@@ -0,0 +1,37 @@
+package io.github.jvitoroliv3ira.fancyurlshortener.redirecting.infrastructure.web;
+
+import java.net.URI;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.command.ResolveRedirectTargetCommand;
+import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.handler.ResolveRedirectTargetHandler;
+import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.result.ResolveRedirectTargetResult;
+import io.github.jvitoroliv3ira.fancyurlshortener.shared.domain.valueobject.ShortCode;
+
+@RestController
+@RequestMapping("/r")
+public class ResolveRedirectTargetController {
+  private final ResolveRedirectTargetHandler handler;
+
+  public ResolveRedirectTargetController(ResolveRedirectTargetHandler handler) {
+    this.handler = handler;
+  }
+
+  @GetMapping("/{code}")
+  public ResponseEntity<ResolveRedirectTargetResponse> redirect(@PathVariable String code) {
+    ResolveRedirectTargetCommand command = new ResolveRedirectTargetCommand(new ShortCode(code));
+
+    ResolveRedirectTargetResult result = handler.handle(command);
+
+    return ResponseEntity
+        .status(HttpStatus.FOUND)
+        .location(URI.create(result.url()))
+        .build();
+  }
+}
---
+++ services/fancy-url-shortener/src/main/java/io/github/jvitoroliv3ira/fancyurlshortener/redirecting/infrastructure/web/ResolveRedirectTargetResponse.java
@@ -0,0 +1,4 @@
+package io.github.jvitoroliv3ira.fancyurlshortener.redirecting.infrastructure.web;
+
+public record ResolveRedirectTargetResponse(String url) {
+}
