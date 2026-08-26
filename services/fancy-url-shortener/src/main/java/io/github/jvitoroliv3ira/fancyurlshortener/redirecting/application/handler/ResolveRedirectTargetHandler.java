7538b0e60d25f8edd52308b6ff1882c19c3b7a24
João Vitor <jvitoroliveira0711@gmail.com>
2026-08-25 22:08:00 -0300

    chore: Aplica novas exceptions

--- services/fancy-url-shortener/src/main/java/io/github/jvitoroliv3ira/fancyurlshortener/redirecting/application/handler/ResolveRedirectTargetHandler.java
+++ services/fancy-url-shortener/src/main/java/io/github/jvitoroliv3ira/fancyurlshortener/redirecting/application/handler/ResolveRedirectTargetHandler.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.command.ResolveRedirectTargetCommand;
+import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.exception.RedirectTargetExpiredException;
+import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.exception.RedirectTargetNotFoundException;
 import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.result.ResolveRedirectTargetResult;
 import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.domain.repository.RedirectTargetLookup;
 import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.domain.valueobject.RedirectTarget;
@@ -31,13 +33,13 @@ public class ResolveRedirectTargetHandler {
     Optional<RedirectTarget> result = redirectTargetLookup.findByShortCode(command.shortCode());
 
     if (result.isEmpty()) {
-      throw new IllegalStateException("Redirect target not found");
+      throw new RedirectTargetNotFoundException();
     }
 
     RedirectTarget redirectTarget = result.get();
 
     if (!redirectTarget.isRedirectableAt(now)) {
-      throw new IllegalStateException("Redirect target is expired");
+      throw new RedirectTargetExpiredException();
     }
 
     return new ResolveRedirectTargetResult(redirectTarget.url());
