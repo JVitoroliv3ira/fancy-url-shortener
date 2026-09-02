44bdbb2ab3646007ed072aba130e3b16a76110df
João Vitor <jvitoroliveira0711@gmail.com>
2026-09-01 21:25:21 -0300

    chore: Renomeia para RedirectController

--- services/fancy-url-shortener/src/main/java/io/github/jvitoroliv3ira/fancyurlshortener/redirecting/infrastructure/web/RedirectController.java
+++ services/fancy-url-shortener/src/main/java/io/github/jvitoroliv3ira/fancyurlshortener/redirecting/infrastructure/web/RedirectController.java
@@ -9,23 +9,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.command.ResolveRedirectTargetCommand;
-import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.handler.ResolveRedirectTargetHandler;
+import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.command.ResolveRedirectCommand;
+import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.handler.ResolveRedirectHandler;
 import io.github.jvitoroliv3ira.fancyurlshortener.redirecting.application.result.ResolveRedirectTargetResult;
 import io.github.jvitoroliv3ira.fancyurlshortener.shared.domain.valueobject.ShortCode;
 
 @RestController
 @RequestMapping("/r")
-public class ResolveRedirectTargetController {
-  private final ResolveRedirectTargetHandler handler;
+public class RedirectController {
+  private final ResolveRedirectHandler handler;
 
-  public ResolveRedirectTargetController(ResolveRedirectTargetHandler handler) {
+  public RedirectController(ResolveRedirectHandler handler) {
     this.handler = handler;
   }
 
   @GetMapping("/{code}")
   public ResponseEntity<Void> redirect(@PathVariable String code) {
-    ResolveRedirectTargetCommand command = new ResolveRedirectTargetCommand(new ShortCode(code));
+    ResolveRedirectCommand command = new ResolveRedirectCommand(new ShortCode(code));
 
     ResolveRedirectTargetResult result = handler.handle(command);
 
