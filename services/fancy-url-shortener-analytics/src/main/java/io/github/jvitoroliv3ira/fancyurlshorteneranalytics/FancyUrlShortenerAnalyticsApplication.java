41c63479e6bf29eeee66a59d5cd3e8559c1cc409
João Vitor <jvitoroliveira0711@gmail.com>
2026-09-05 18:17:59 -0300

    chore: Adiciona codigo inicial do serviço de analytics

---
+++ services/fancy-url-shortener-analytics/src/main/java/io/github/jvitoroliv3ira/fancyurlshorteneranalytics/FancyUrlShortenerAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package io.github.jvitoroliv3ira.fancyurlshorteneranalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancyUrlShortenerAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FancyUrlShortenerAnalyticsApplication.class, args);
+	}
+
+}
---
+++ services/fancy-url-shortener-analytics/src/test/java/io/github/jvitoroliv3ira/fancyurlshorteneranalytics/FancyUrlShortenerAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jvitoroliv3ira.fancyurlshorteneranalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancyUrlShortenerAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
